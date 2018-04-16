cbead97b1f505bddd95a5143ac98c3c460049caa
Josh Felix <jfelix76@gmail.com>
2018-04-16 08:06:35 -0400

    switched up intro

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -9,12 +9,7 @@ export class IntroComponent implements OnInit {
 
   introMessage: string = `
   
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore magna 
-    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-    Duis aute irure dolor in reprehenderit in voluptate velit 
-    esse cillum dolore eu fugiat nulla...
+    Create My Cookbook wants to help you pass on your legacy, make a difference or establish your brand...
   
   `;
 
